1f1d99f04fe1219eddfe9370968a62a0ec3c6c08
Dan Funk <daniel.h.funk@gmail.com>
2018-12-19 16:27:34 -0500

    Adding stuff to build the war file.

---
+++ make_war.sh
@@ -0,0 +1,18 @@
+# A simple script for turning this html content into a war file.
+# We have the ability to deploy war files, and I don't want to
+# complicate the deployment process by adding Apache to our server,
+# thus this little complication.  Just run this, and take the generated
+# war file, and place it on the server.
+
+rm -rf war
+mkdir war
+cp index.html war
+cp -r WEB-INF war
+cp -r img war
+cp -r css war
+cp -r vendor war
+cd war
+zip -r landing.war *
+mv landing.war ..
+cd ..
+rm -rf war
